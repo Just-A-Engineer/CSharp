12792f08dfe2d6e803113cea9681eae2ef36c604
Justin McCoy <31171668+Just-A-Engineer@users.noreply.github.com>
2022-04-07 22:56:03 -0600

    Updated multiple methods and added app.exe
    Updated application to when you select either Cube or Square Root, it doesn't ask for a second number. Because it is not needed. Also added comments throughout code as a whole. Added app.exe.

--- CalculatorCS/app.cs
+++ CalculatorCS/app.cs
@@ -4,17 +4,28 @@ namespace HelloWorld {
   class MainClass {
     // Main Method
     public static void Main (string[] args) {
+      //Array of possible operations in the calculator
       string[] operations = {"Add", "Subtract", "Multiply", "Divide", "Cube", "Square Root"};
-      //Could also use Convert.ToDouble(Console.ReadLine()); in place of double.Parse.
-      Console.WriteLine("What would you like your first number to be?");
-      double x = double.Parse(Console.ReadLine());
-      Console.WriteLine("What would you like your second number to be? (If Cubing or Square Rooting, second number does not matter)");
-      double y = double.Parse(Console.ReadLine());
       //Grabs the operator from the user
       Console.WriteLine("What would you like to do? " + string.Join(", ", operations));
       string operate = Console.ReadLine();
-      //NewMath method is given an operator, and two numbers and does the math for you
-      NewMath(operate, x, y);
+      //Checks to see if operate variable is equal to two operations.
+      //In these two cases, the second number does not matter, so it asks you for a single number.
+      if(operate == "Cube" | operate == "Square Root") {
+        //Could also use Convert.ToDouble(Console.ReadLine()); in place of double.Parse.
+        Console.WriteLine("What would you like your number to be?");
+        double x = double.Parse(Console.ReadLine());
+        //Second number in the NewMath method does not matter in these two cases. 
+        NewMath(operate, x, 1);
+      } else {
+        //Could also use Convert.ToDouble(Console.ReadLine()); in place of double.Parse.
+        Console.WriteLine("What would you like your first number to be?");
+        double x = double.Parse(Console.ReadLine());
+        Console.WriteLine("What would you like your second number to be?");
+        double y = double.Parse(Console.ReadLine());
+        //Takes operate, as well as x, and y, and does the Math for you
+        NewMath(operate, x, y);
+      }
     }
     // NewMath method
     public static void NewMath(string operate, double x, double y) {
